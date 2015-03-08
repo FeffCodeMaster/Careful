47d150faf7117f5784fe972d0da0cba430fbe654
FeffCodeMaster <CodeFeff@Fredriks-MBP.lan>
2015-03-08 21:17:40 +0100

    Add the gym scen
    Added the new gym scen and also all the file required for a mac based
    unity project

--- Assets/updatespersecond.cs
+++ Assets/updatespersecond.cs
@@ -5,13 +5,15 @@ public class updatespersecond : MonoBehaviour {
 
     GUIText guiText;
 	void Start () {
-        guiText = gameObject.GetComponent<GUIText>();
+        
+		guiText = gameObject.GetComponent<GUIText>();
+		Application.targetFrameRate = 60;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         var framRate = (1.0f / Time.deltaTime);
         this.guiText.text = framRate.ToString();
-        Application.targetFrameRate = 60;
+
 	}
 }
